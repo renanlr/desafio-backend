0467f39c24d1bd03956148dd739bc6ed2359aaed
Renan Lobato Rheinboldt <renan.lobato.rheinboldt@gmail.com>
2020-03-27 18:53:24 -0300

    add views for Trip

--- api/views.py
+++ api/views.py
@@ -1,3 +1,51 @@
-from django.shortcuts import render
+from rest_framework import mixins
+from rest_framework import generics
+from rest_framework.response import Response
+from .models import Trip
+from .serializers import TripSerializer
+from rest_framework.permissions import IsAuthenticated
+from api.permissions import IsOwnerOrReadOnly
+from rest_framework.decorators import api_view
+from rest_framework.reverse import reverse
+from rest_framework import status
+
 
 # Create your views here.
+
+@api_view(['GET'])
+def api_root(request):
+    return Response({
+        'trips': reverse('trip-list', request=request, format=format)
+    })
+
+
+class TripList(mixins.ListModelMixin,
+               mixins.CreateModelMixin,
+               generics.GenericAPIView):
+    permission_classes = [IsAuthenticated, IsOwnerOrReadOnly]
+    serializer_class = TripSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+    def get_queryset(self):
+        """
+        This view should return a list of all the trips
+        for the currently authenticated user.
+        """
+        user = self.request.user
+        return Trip.objects.filter(owner=user)
+
+
+class TripDetail(mixins.RetrieveModelMixin,
+                 mixins.UpdateModelMixin,
+                 mixins.DestroyModelMixin,
+                 generics.GenericAPIView):
+    permission_classes = [IsAuthenticated, IsOwnerOrReadOnly]
+    queryset = Trip.objects.all()
+    serializer_class = TripSerializer
+
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+
