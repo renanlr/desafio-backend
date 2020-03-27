5205f5c7ef3ebd06d108c806316b68a5dd74c622
Renan Lobato Rheinboldt <renan.lobato.rheinboldt@gmail.com>
2020-03-27 18:53:48 -0300

    import api.urls

--- desafioBackend/urls.py
+++ desafioBackend/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from rest_framework.authtoken.views import obtain_auth_token
+from django.conf.urls import url,include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url(r'^', include('api.urls')),
+    path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
 ]
